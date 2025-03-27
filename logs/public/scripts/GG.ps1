b7dc651567eb9a758f73fd0c4a0162bcc485a6e9
iamsven2005 <iamsven2005@gmail.com>
2025-03-27 17:05:02 +0800

    I forgot

--- logs/public/scripts/GG.ps1
+++ logs/public/scripts/GG.ps1
@@ -1,38 +1,23 @@
-# # Collect disk info
-# $drives = Get-PSDrive -PSProvider 'FileSystem'
-# $diskInfo = @()
+# === Collect Disk Info ===
+$drives = Get-PSDrive -PSProvider 'FileSystem'
+$diskInfo = @()
 
-# foreach ($drive in $drives) {
-#     $driveSize = $drive.Used + $drive.Free
-#     $driveFree = $drive.Free
-#     $driveUsed = $driveSize - $driveFree
+foreach ($drive in $drives) {
+    $driveSize = $drive.Used + $drive.Free
+    $driveFree = $drive.Free
+    $driveUsed = $driveSize - $driveFree
 
-#     $diskInfo += [PSCustomObject]@{
-#         name    = $drive.Name
-#         label   = $drive.DisplayRoot
-#         totalGB = [math]::Round($driveSize / 1GB, 2)
-#         usedGB  = [math]::Round($driveUsed / 1GB, 2)
-#         freeGB  = [math]::Round($driveFree / 1GB, 2)
-#     }
-# }
+    $diskInfo += [PSCustomObject]@{
+        name    = $drive.Name
+        label   = $drive.DisplayRoot
+        totalGB = [math]::Round($driveSize / 1GB, 2)
+        usedGB  = [math]::Round($driveUsed / 1GB, 2)
+        freeGB  = [math]::Round($driveFree / 1GB, 2)
+    }
+}
 
-# $payload = [PSCustomObject]@{
-#     hostname  = $env:COMPUTERNAME
-#     timestamp = (Get-Date).ToString("s")
-#     disks     = $diskInfo
-# }
-
-# $jsonPayload = $payload | ConvertTo-Json -Depth 5
-
-# # Send to your Next.js server
-# Invoke-RestMethod -Uri "http://192.168.1.102:3000/api/deviceinfo" `
-#                   -Method Post `
-#                   -ContentType "application/json" `
-#                   -Body $jsonPayload
-# Get current running processes
+# === Collect Process Info ===
 $processes = Get-Process | Select-Object Id, ProcessName, CPU, WorkingSet64
-
-# Format process info into a list of objects
 $procList = @()
 
 foreach ($proc in $processes) {
@@ -44,19 +29,18 @@ foreach ($proc in $processes) {
     }
 }
 
-# Final payload
-$payload = @{
+# === Final Combined Payload ===
+$payload = [PSCustomObject]@{
     hostname  = $env:COMPUTERNAME
     timestamp = (Get-Date).ToString("s")
     disks     = $diskInfo
     processes = $procList
 }
 
-# Convert to JSON
+# === Send to API ===
 $jsonPayload = $payload | ConvertTo-Json -Depth 5
 
-# Send to your API endpoint
-Invoke-RestMethod -Uri "http://192.168.1.102:3000/api/pid" `
+Invoke-RestMethod -Uri "http://192.168.1.26:3000/api/deviceinfo" `
                   -Method Post `
                   -ContentType "application/json" `
                   -Body $jsonPayload
